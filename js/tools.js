ba92389234ce0f345aae98be768ae7ed559c297d
JIEMO <lv256@qq.com>
2024-01-08 21:36:58 +0800

    update

--- js/tools.js
+++ js/tools.js
@@ -164,16 +164,27 @@ function BrushVideos(a,b){
         // 生成随机坐标和滑动时间
         var startX = random(800/1440*device.width, 1100/1440*device.width);
         var startY = random(2210/3168*device.height, 2399/3168*device.height);
-        var endX = random(900/1440*device.width, 1020/1440*device.width);
-        var endY = random(200/3168*device.height, 399/3168*device.height);
-        var duration = random(420, 720);
+            var endX = random(900 / 1440 * device.width, 1020 / 1440 * device.width);
+            var endY = random(200 / 3168 * device.height, 399 / 3168 * device.height);
+            var duration = random(420, 720);
 
-        swipe(startX, startY, endX, endY, duration);
-        toastLog(APP_name + "计数器：" + (i + 1));
-        // 生成2.6到8.7之间的随机数
-        sleep(random(2600, 8700));
-    }
+            swipe(startX, startY, endX, endY, duration);
+            toastLog(APP_name + "计数器：" + (i + 1));
+            // 生成2.6到8.7之间的随机数
+            sleep(random(2600, 8700));
+        }
 }
+
+//浮窗当前APP
+function FloatingCurrentAPP() {
+    // 打开多任务视图
+    recents();
+    desc("更多").findOne().click();
+    sleep(150);
+    click("浮窗");
+}
+
+
 
 
 
